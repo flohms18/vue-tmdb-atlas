6d1c3b4faefd33269bb80b275e3e9471088404b2
flohms18 <trouilletflorent@outlook.com>
2026-08-17 20:10:19 +0200

    Tested javascript function

--- src/templates/Home.vue
+++ src/templates/Home.vue
@@ -1,3 +1,20 @@
+<script setup>
+import { onMounted } from 'vue';
+
+
+onMounted(() => {
+    function james(bond){
+        bond = 'Casino Royale';
+        console.log(bond)
+    }
+
+    james()
+})
+
+
+</script>
+
+
 <template>
     <div>
         <h1 class="text-3xl font-bold underline">

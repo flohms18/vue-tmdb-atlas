c4b3412829ad97b5a5ea6761d8d00bc1522236c8
flohms18 <trouilletflorent@outlook.com>
2026-08-18 21:05:06 +0200

    Display data on html

--- src/templates/Home.vue
+++ src/templates/Home.vue
@@ -1,36 +1,22 @@
 <script setup>
-import { onMounted } from 'vue';
 import axios from 'axios';
+import { ref } from 'vue'
+
+const item = ref([])
 
 
-
-
-onMounted(() => {
-    function james(bond){
-        bond = 'Casino Royale';
-        console.log(import.meta.env.VITE_TMDB_API_KEY)
-    }
-
-    james()
-
-
-    
-
-    
-})
-
-function callAPI(api_key) {
-        api_key = import.meta.env.VITE_TMDB_API_KEY
+function callAPI() {
+        const api_key = import.meta.env.VITE_TMDB_API_KEY
         const options = {
             method : 'GET',
-            url: 'https://api.themoviedb.org/3/search/movie?query=' + searchMovie + '&include_adult=false&language=en-US&page=1',
+            url: 'https://api.themoviedb.org/3/search/movie?query=' + searchMovie.value + '&include_adult=false&language=en-US&page=1',
             headers : {
                 accept : 'application/json', Authorization : 'Bearer ' + api_key
             }
         };
         axios
             .request(options)
-            .then(res => console.log(res.data))
+            .then(res => item.value = res.data.results)
             .catch(err => console.error(err));
     }
 
@@ -43,10 +29,14 @@ const searchMovie = defineModel()
 
 <template>
     <div class="flex flex-col w-screen text-center py-5 text-4xl">
-        <h1 class="text-blue-brand font-display">TMDB App</h1>  
-           <span>My input</span> <input v-model="model">
-           {{ model }}
-            <button type="button" @click="callAPI()">Click Me!</button> 
+        <h1 class="text-blue-brand font-display">TMDB App</h1>
+           <span>My input</span> <input v-model="searchMovie">
+            <button type="button" @click="callAPI()">Click Me!</button>
+            <ul>
+                <li v-for="movie in item" :key="movie.id">
+                    {{ movie.title }} ({{ movie.release_date?.slice(0, 4) }})
+                </li>
+            </ul>
 
     </div>
 </template>

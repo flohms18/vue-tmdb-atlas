1da1da277f89753422f799ae5f8881d55327c94b
flohms18 <trouilletflorent@outlook.com>
2026-08-19 12:19:15 +0200

    Improve design

--- src/templates/Home.vue
+++ src/templates/Home.vue
@@ -19,9 +19,13 @@ function callAPI() {
             .then(res => item.value = res.data.results)
             .then(res => console.log(res))
             .catch(err => console.error(err));
+        
     }
 
 const searchMovie = defineModel()
+
+
+
 
 
 
@@ -29,10 +33,13 @@ const searchMovie = defineModel()
 
 
 <template>
-    <div class="flex flex-col w-screen text-center py-5 text-4xl">
-        <h1 class="text-blue-brand font-display">TMDB App</h1>
-           <span>My input</span> <input v-model="searchMovie">
-            <button type="button" @click="callAPI()">Click Me!</button>
+    <div class="flex flex-col items-center py-5 w-screen bg-dark-brand items-center h-screen">
+        <h1 class="text-blue-brand font-display text-center text-4xl py-20">TMDB App</h1>
+           <p class="text-white-brand text-center font-display pt-5">Enter the movie's name</p> 
+           <input class="max-w-2xs placeholder:text-gray-500 w-3xs pl-3 text-white-brand outline-gray-600 mt-5 rounded-md" v-model="searchMovie" placeholder="Dune">
+            <button type="button" @click="callAPI()" class="text-blue-brand font-display pt-5">Search</button>
+            
+            
             <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4 text-left">
                 <div v-for="movie in item" :key="movie.id" class="flex flex-col">
                     <img
@@ -46,9 +53,9 @@ const searchMovie = defineModel()
                     </div>
                     <p class="mt-2 text-base font-display truncate">{{ movie.title }}</p>
                     <p class="text-sm text-dark-brand/70">{{ movie.release_date }}</p>
+                    <button @click="">Watch the trailer</button>
                 </div>
             </div>
-
     </div>
 
     

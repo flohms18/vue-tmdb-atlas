085cd69547e547af9faa87eea8895823cbf63c1f
flohms18 <trouilletflorent@outlook.com>
2026-08-19 12:52:25 +0200

    Works project

--- src/templates/Home.vue
+++ src/templates/Home.vue
@@ -3,11 +3,12 @@ import axios from 'axios';
 import { ref } from 'vue'
 
 const item = ref([])
-
+const trailerKey = ref(null)
+const showTrailerModal = ref(false)
 
 function callAPI() {
         const api_key = import.meta.env.VITE_TMDB_API_KEY
-        const options = {
+        const options_search = {
             method : 'GET',
             url: 'https://api.themoviedb.org/3/search/movie?query=' + searchMovie.value + '&include_adult=false&language=en-US&page=1',
             headers : {
@@ -15,12 +16,41 @@ function callAPI() {
             }
         };
         axios
-            .request(options)
+            .request(options_search)
             .then(res => item.value = res.data.results)
             .then(res => console.log(res))
             .catch(err => console.error(err));
-        
     }
+
+function watchTrailer(movieId) {
+    const api_key = import.meta.env.VITE_TMDB_API_KEY
+    const options_trailer = {
+        method : 'GET',
+        url: `https://api.themoviedb.org/3/movie/${movieId}/videos`,
+        headers : {
+            accept : 'application/json', Authorization : 'Bearer ' + api_key
+        }
+    };
+    axios
+        .request(options_trailer)
+        .then(res => {
+            const videos = res.data.results
+            const video = videos.find(v => v.type === 'Trailer' && v.site === 'YouTube')
+                || videos.find(v => v.site === 'YouTube')
+            if (video) {
+                trailerKey.value = video.key
+                showTrailerModal.value = true
+            } else {
+                console.error('No trailer available for this movie')
+            }
+        })
+        .catch(err => console.error(err));
+}
+
+function closeTrailer() {
+    showTrailerModal.value = false
+    trailerKey.value = null
+}
 
 const searchMovie = defineModel()
 
@@ -53,7 +83,27 @@ const searchMovie = defineModel()
                     </div>
                     <p class="mt-2 text-base font-display truncate text-blue-brand font-display">{{ movie.title }}</p>
                     <p class="text-sm text-white-brand font-display">{{ movie.release_date }}</p>
-                    <button class="text-white-brand">Watch the trailer</button>
+                    <button class="text-white-brand" @click="watchTrailer(movie.id)">Watch the trailer</button>
+                </div>
+            </div>
+
+            <div
+                v-if="showTrailerModal"
+                class="fixed inset-0 z-50 flex items-center justify-center bg-black/70"
+                @click.self="closeTrailer()"
+            >
+                <div class="bg-dark-brand border border-blue-brand rounded-md p-4 w-11/12 max-w-3xl">
+                    <div class="flex justify-end mb-2">
+                        <button class="text-blue-brand font-display" @click="closeTrailer()">Close</button>
+                    </div>
+                    <div class="relative w-full aspect-video">
+                        <iframe
+                            class="absolute inset-0 w-full h-full rounded-md"
+                            :src="`https://www.youtube.com/embed/${trailerKey}`"
+                            allow="autoplay; encrypted-media"
+                            allowfullscreen
+                        ></iframe>
+                    </div>
                 </div>
             </div>
     </div>
